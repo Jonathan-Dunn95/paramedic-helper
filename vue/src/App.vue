30dfdabb43a197b3050c2b666bb47021668bd0cc
Jonathan <jonathandunn95@outlook.com>
2023-08-30 03:13:44 -0400

    Navigation bar added

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <nav-bar />
   <router-view />
 </template>
 
+<script>
+import navBar from './components/NavigationBar.vue';
+
+export default {
+  components: {
+    navBar
+  }
+}
+</script>
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap');
+
+body {
+  margin: 0;
+}
 #app {
+  width: 100vw;
+  min-height: 100vh;
+  max-width: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  background-color: black;
 }
 </style>
---
+++ vue/src/components/NavigationBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="navigation-bar">
+      <nav>
+        <ul class="nav-list">
+          <li @click="navigateTo('parents')">Protocols</li>
+          <li @click="navigateTo('item-store')">Drug Formulary</li>
+          <li @click="navigateTo('closet')">Procedures</li>
+          <li @click="navigateTo('login')">Tools</li>
+          <!-- <li @click="navigateTo('logout')">Contact</li> -->
+        </ul>
+      </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "nav-bar",
+    methods: {
+        navigateTo(view) {
+            this.$router.push({ name: view });
+        }
+    },
+}
+</script>
+
+<style scoped>
+.navigation-bar {
+  background-color: gray;
+  padding: 1rem 0;
+  border-radius: 0.5rem;
+}
+.nav-list li {
+  display: inline;
+  font-size: 1.5rem;
+  font-weight: bolder;
+  color: white;
+  cursor: pointer;
+  transition: background-color 0.3s, color 0.3s;
+  position: relative;
+  padding: 2rem;
+}
+.nav-list li:hover {
+  color: black;
+  background-color: white;
+}
+</style>
