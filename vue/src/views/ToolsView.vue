eb71282d67bbc875c1e725c24c2f23b25ae3fdaf
Jonathan <jonathandunn95@outlook.com>
2023-09-10 22:53:35 -0400

    Added ideas to add into Tools page

--- vue/src/views/ToolsView.vue
+++ vue/src/views/ToolsView.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>Tools List</h1>
+    <h2>Drip Calculations</h2>
+    <h2>TBSA burn percentage</h2>
+    <h2>Pediatric dosages?</h2>
+    <h2>Pediatric VS ranges by age</h2>
+    <h2>RSI Cheat Sheet</h2>
   </div>
 </template>
 
